297fbefd87b72cd5d2475dc033f27e04b6476338
peyman <lord.akinak@gmail.com>
2020-12-21 05:00:15 +0330

    better metrics crud;

--- src/CRUD/MetricsCRUDProvider.php
+++ src/CRUD/MetricsCRUDProvider.php
@@ -3,10 +3,13 @@
 
 namespace Larapress\Reports\CRUD;
 
+use Illuminate\Support\Facades\Auth;
 use Larapress\CRUD\Services\BaseCRUDProvider;
 use Larapress\CRUD\Services\ICRUDProvider;
 use Larapress\CRUD\Services\IPermissionsMetadata;
+use Larapress\Profiles\Models\FormEntry;
 use Larapress\Reports\Models\MetricCounter;
+use Larapress\ECommerce\IECommerceUser;
 
 class MetricsCRUDProvider implements ICRUDProvider, IPermissionsMetadata
 {
@@ -35,4 +38,32 @@ class MetricsCRUDProvider implements ICRUDProvider, IPermissionsMetadata
         'key' => 'like:key',
     ];
     public $filterDefaults = [];
+
+
+    /**
+     * @param Builder $query
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function onBeforeQuery($query)
+    {
+        /** @var IECommerceUser $user */
+        $user = Auth::user();
+        if (! $user->hasRole(config('larapress.profiles.security.roles.super-role'))) {
+            if ($user->hasRole(config('larapress.ecommerce.lms.owner_role_id'))) {
+                $ownerEntries = collect($user->getOwenedProductsIds())->map(function ($id) {
+                    return 'product[.]'.$id.'[.].*';;
+                })->toArray();
+                $query->whereRaw('metrics_counters.key REGEXP \''.implode('|', $ownerEntries).'\'');
+            } else if ($user->hasRole(config('larapress.ecommerce.lms.support_role_id'))) {
+                $query->where('key', 'LIKE', '%.'.$user->id);
+            } else {
+                $query->whereHas('domains', function ($q) use ($user) {
+                    $q->whereIn('id', $user->getAffiliateDomainIds());
+                });
+            }
+        }
+
+        return $query;
+    }
 }
